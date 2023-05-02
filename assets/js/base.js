8b8281574e0cbaf143d8aac9cd2d2172d372ca91
GuaiJieYo <guaijieaz@gmail.com>
2023-05-02 20:11:36 +0800

    add: 添加loading动画

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,25 @@
+// 监听页面加载完成事件
+window.addEventListener('load', function () {
+    // 获取loading元素
+    const loadingbox = document.querySelector('#loading');
+    // 设置初始透明度为1
+    let opacity = 1;
+    // 设置定时器，每50毫秒执行一次
+    const timer = setInterval(() => {
+        // 当透明度小于等于0.1时，清除定时器，隐藏loading元素，并触发自定义事件
+        if (opacity <= 0.1) {
+            clearInterval(timer);
+            loadingbox.style.display = 'none';
+            window.dispatchEvent(new Event('OverLoading'));
+        }
+        // 设置loading元素透明度
+        loadingbox.style.opacity = opacity;
+        // 透明度每次减小原来的30%
+        opacity -= opacity * 0.3;
+    }, 50);
+});
+
+
 // 请不要删除这段LOG
 console.log(
     '%c Made By GuaiJie %c v0.0.1 ',
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,48 +6,53 @@ var prevBtn = carousel.querySelector('.prev');
 var nextBtn = carousel.querySelector('.next');
 // 初始化当前显示的图片索引
 var currentImageIndex = 0;
+// 自动播放定时器
+var carouseltimer = null;
 
 // 自动播放函数，每隔3秒切换一张图片
 function autoPlay() {
-    // 隐藏当前显示的图片
-    images[currentImageIndex].classList.remove('active');
-    // 切换到下一张图片
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    // 显示下一张图片
-    images[currentImageIndex].classList.add('active');
+  // 隐藏当前显示的图片
+  images[currentImageIndex].classList.remove('active');
+  // 切换到下一张图片
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  // 显示下一张图片
+  images[currentImageIndex].classList.add('active');
 }
 
-// 绑定上一张按钮的点击事件
-prevBtn.addEventListener('click', function () {
-    // 隐藏当前显示的图片
-    images[currentImageIndex].classList.remove('active');
+// 切换图片函数
+function switchImage(index) {
+  // 隐藏当前显示的图片
+  images[currentImageIndex].classList.remove('active');
+  // 切换到指定索引的图片
+  currentImageIndex = index;
+  // 显示指定索引的图片
+  images[currentImageIndex].classList.add('active');
+}
+
+// 绑定上一张和下一张按钮的点击事件
+carousel.addEventListener('click', function (event) {
+  if (event.target.matches('.prev')) {
     // 切换到上一张图片
-    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-    // 显示上一张图片
-    images[currentImageIndex].classList.add('active');
-});
-
-// 绑定下一张按钮的点击事件
-nextBtn.addEventListener('click', function () {
-    // 隐藏当前显示的图片
-    images[currentImageIndex].classList.remove('active');
+    switchImage((currentImageIndex - 1 + images.length) % images.length);
+  } else if (event.target.matches('.next')) {
     // 切换到下一张图片
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    // 显示下一张图片
-    images[currentImageIndex].classList.add('active');
+    switchImage((currentImageIndex + 1) % images.length);
+  }
 });
 
-// 自动播放定时器
-var carouseltimer = setInterval(autoPlay, 3000);
-
-// 绑定上一张和下一张按钮的鼠标移入移出事件
+// 绑定轮播图容器的鼠标移入移出事件
 carousel.addEventListener('mouseover', function (event) {
-    if (event.target.matches('.prev, .next')) {
-        clearInterval(carouseltimer);
-    }
+  if (event.target.matches('.prev, .next')) {
+    clearInterval(carouseltimer);
+  }
 });
 carousel.addEventListener('mouseout', function (event) {
-    if (event.target.matches('.prev, .next')) {
-        carouseltimer = setInterval(autoPlay, 3000);
-    }
+  if (event.target.matches('.prev, .next')) {
+    carouseltimer = setInterval(autoPlay, 3000);
+  }
+});
+
+// 开始自动播放
+window.addEventListener('OverLoading', () => {
+  carouseltimer = setInterval(autoPlay, 3000);
 });
