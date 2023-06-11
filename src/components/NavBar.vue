7127d988c515164c2591a72d13a07b1d2a54235c
GuaiJieYo <guaijieaz@gmail.com>
2023-06-11 14:45:48 +0800

    fix: 修复navbar错误

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,111 +1,105 @@
 <template>
-    <header>
-        <div class="header">
-            <a href="#/" class="left">
-                <i class="iconfont icon-cloud"></i>
-                <p>YunLiuCraft</p>
-            </a>
-            <div class="right">
-                <a active href="#/">
-                    <i class="iconfont icon-index"></i>首页
-                </a>
-                <a href="#/join">
-                    <i class="iconfont icon-plus"></i>加入
-                </a>
-                <a href="#/rule">
-                    <i class="iconfont icon-rule"></i>规则
-                </a>
-                <a href="#/help">
-                    <i class="iconfont icon-help"></i>帮助
-                </a>
-                <a href="#/feedback">
-                    <i class="iconfont icon-feedback"></i>反馈
-                </a>
-            </div>
-        </div>
-    </header>
+  <header>
+    <div class="header">
+      <router-link to="/" class="left">
+        <i class="iconfont icon-cloud"></i>
+        <p>YunLiuCraft</p>
+      </router-link>
+      <div class="right">
+        <router-link exact-active-class="active" :to="item.link" v-for="(item, index) in navList" :key="index" :href="item.link">
+          <i class="iconfont" :class="item.icon"></i>{{ item.title }}
+        </router-link>
+      </div>
+    </div>
+  </header>
 </template>
-
+  
 <script setup>
-
+const navList = [
+  { title: '首页', link: '/', icon: 'icon-index' },
+  { title: '加入', link: '/join', icon: 'icon-plus' },
+  { title: '规则', link: '/rule', icon: 'icon-rule' },
+  { title: '帮助', link: '/help', icon: 'icon-help' },
+  { title: '反馈', link: '/feedback', icon: 'icon-feedback' }
+]
 </script>
 
 <style scoped>
 header {
-    width: 100vw;
-    height: 80px;
-    background: transparent;
-    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16),
-        0 2px 10px 0 rgba(0, 0, 0, 0.12);
-    transition: all .3s;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1;
+  width: 100vw;
+  height: 80px;
+  background: transparent;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16),
+    0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  transition: all .3s;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
 }
 
 header .header {
-    height: 100%;
-    margin: 0 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  height: 100%;
+  margin: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 header .header .left {
-    display: flex;
-    align-items: center;
-    color: #000;
+  display: flex;
+  align-items: center;
+  color: #fff;
 }
 
 header .header .left p {
-    font-size: 2.3rem;
+  font-size: 2.3rem;
 }
 
 header .header .left i {
-    margin-right: 6px;
-    font-size: 3rem;
+  margin-right: 6px;
+  font-size: 3rem;
 }
 
 header .header .right a i {
-    margin: 0 2px;
+  margin: 0 2px;
 }
 
 header .header .right a {
-    color: #000;
-    margin: 0 10px;
-    font-size: 1.6rem;
-    vertical-align: middle;
-    position: relative;
+  color: #fff;
+  margin: 0 10px;
+  font-size: 1.6rem;
+  vertical-align: middle;
+  position: relative;
 }
 
-header .header .right a[active]:after {
-    display: block;
-    width: 90%;
-    background-color: #000;
-    height: 2px;
-    position: absolute;
-    bottom: -3px;
-    left: 50%;
-    transform: translateX(-50%);
+header .header .right a.active:after {
+  display: block;
+  width: 90%;
+  background-color: #fff;
+  height: 2px;
+  position: absolute;
+  bottom: -3px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 
 header .header .right a:after {
-    display: block;
-    content: '';
-    width: 0;
-    height: 2px;
-    position: absolute;
-    bottom: -3px;
-    left: 50%;
-    transform: translateX(-50%);
-    transition: all 0.3s;
+  display: block;
+  content: '';
+  width: 0;
+  height: 2px;
+  position: absolute;
+  bottom: -3px;
+  left: 50%;
+  transform: translateX(-50%);
+  transition: all 0.3s;
 }
 
 header .header .right a:hover:after {
-    display: block;
-    content: '';
-    width: 90%;
-    background-color: #000;
+  display: block;
+  content: '';
+  width: 90%;
+  background-color: #fff;
 }
 </style>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,18 +1,74 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
 // 导入组件
 import Home from '../components/main.vue'
+
+// 路由列表
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: {
+      title: '主页'
+    }
+  },
+  {
+    path: '/join',
+    name: 'join',
+    component: Home,
+    meta: {
+      title: '加入我们'
+    }
+  },
+  {
+    path: '/rule',
+    name: 'rule',
+    component: Home,
+    meta: {
+      title: '规则'
+    }
+  },
+  {
+    path: '/help',
+    name: 'help',
+    component: Home,
+    meta: {
+      title: '帮助'
+    }
+  },
+  {
+    path: '/feedback',
+    name: 'feedback',
+    component: Home,
+    meta: {
+      title: '反馈'
+    }
+  },
+  {
+    path: '/404',
+    name: 'NotFound',
+    component: Home,
+    meta: {
+      title: '未知页面'
+    },
+  }
+]
 
 // 创建路由实例
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-  ],
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    if (to.meta.title) {
+      window.document.title = `${to.meta.title} - YunLiuCraft`;
+    }
+  } else {
+    window.document.title = 'YunLiuCraft - 新征程！'
+  }
+  next()
 })
 
 export default router
